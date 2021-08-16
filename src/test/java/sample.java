76cf1835f9d15c06fc9e413e1ed09a758165a4e4
SathishThiru <59684620+SathishThiru@users.noreply.github.com>
2021-08-16 11:13:37 +0530

    Update sample.java

--- src/test/java/sample.java
+++ src/test/java/sample.java
@@ -17,4 +17,15 @@ public class sample {
 	{
 		System.out.println("b");
 	}
+	
+	@AfterTest
+	public static void setUpResults(ITestResult result)
+	{
+		//we can set parameters to the test result. These parameters can later be used in Reporting(like extent reports)
+		Object[] parameters = new Object[1];
+		paramters[0]="Test case id number";
+		result.setParameters(parameters);
+		
+		
+	}
 }
