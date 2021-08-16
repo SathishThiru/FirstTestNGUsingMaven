6ae53472acc5b70f41d9d3217f22f7a48c4d3bc5
SathishThiru <59684620+SathishThiru@users.noreply.github.com>
2021-08-16 11:25:39 +0530

    Update RetryAnalyzerFromHexa.java

--- src/test/java/IAnnotationTransformer/RetryAnalyzerFromHexa.java
+++ src/test/java/IAnnotationTransformer/RetryAnalyzerFromHexa.java
@@ -1,1 +1,33 @@
+public RetryAnalyzerFromHexa implements IRetryAnalyzer
+{
+static int counter=0;
+static int maxCount=5;
+public boolean retry(ITestResult result)
+{
+if(!result.isSuccess())
+{
+if(counter<maxCount)
+{
+counter++;
+Object[] parameters = result.getParamteres();
+//lets say that the index 0 of the parameters contains test case id
+logger.info("retry " +(counter+1)+" for the test case id"+parameters[0]);
+return true; //Enables testng to rerun test case
+}
+else
+{
+counter=0;//reset counter
+result.setStatus(ITestResult.FAILURE);
 
+}
+}
+else
+{
+counter=0;//reset counter
+result.setStatus(ITestResult.SUCCESS);
+}
+return false;
+}
+
+
+}
